4543a06e77da4c0da43a47c5d1cffe97d68299ce
Marcus <pths411027@gmail.com>
2024-06-23 18:56:39 +0800

    feat: init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
-
 export default function RootLayout({
   children,
 }: Readonly<{
