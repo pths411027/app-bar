40795daddf348cf1d9d608af016d02224aa7dea5
Marcus <pths411027@gmail.com>
2024-06-23 18:53:51 +0800

    feat: init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import Head from "next/head";
 import { Inter } from "next/font/google";
-
+import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -12,9 +12,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <title>App Dock</title>
+        <title>App Bar</title>
         <link rel="icon" type="image/x-icon" href="/favicon.io" />
-        <meta name="description" content="App Dock like mac os" />
+        <script src="https://kit.fontawesome.com/49bdd08b45.js"></script>
+        <meta name="description" content="App Bar effect" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,31 @@
 "use client";
-import { useState, useRef } from "react";
-import { handleCountSize } from "./logic";
+import { useState } from "react";
 import styles from "./Page.module.css";
 import { apps } from "./config";
+
 export default function Home() {
-  const ref = useRef<HTMLDivElement>(null);
-  const rowRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const [location, setLocation] = useState(-1);
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (!ref.current) return;
-    const rect = ref.current.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    let accumulatedWidth = 5;
-    for (let i = 0; i < apps.length; i++) {
-      if (rowRefs.current[i]) {
-        const elementWidth =
-          rowRefs.current[i]!.getBoundingClientRect().width + 10 || 0;
-        accumulatedWidth += elementWidth;
-      }
-      if (accumulatedWidth > x) {
-        setLocation(i);
-        break;
-      }
-    }
-  };
+  const [hoverIndex, setHoverIndex] = useState(-1);
 
   return (
     <main className={styles.container}>
-      <div className={styles.app_title}>Application dock</div>
-      <div
-        ref={ref}
-        className={styles.dock_container}
-        onMouseMove={(e) => handleMouseMove(e)}
-        onMouseLeave={() => setLocation(-1)}
-      >
+      <h1 className={styles.app_title}>App Bar</h1>
+      <div className={styles.bar_container}>
         {apps.map((app, index) => {
-          const { size, marginTop } = handleCountSize(index, location);
+          const isHovered = hoverIndex === index;
           return (
             <div
-              key={app}
-              ref={(ref) => {
-                rowRefs.current[index] = ref;
-              }}
-              className={styles.dock_item}
-              style={{
-                width: size + "px",
-                height: size + "px",
-                marginTop: marginTop + "px",
-              }}
+              key={index}
+              className={styles.bar_item}
+              onMouseEnter={() => setHoverIndex(index)}
+              onMouseLeave={() => setHoverIndex(-1)}
             >
-              <img
-                className={styles.img}
-                src={`/img/${app}.png`}
-                alt={app}
-                width="100%"
-                height="100%"
-              />
+              <i
+                className={app}
+                style={{
+                  marginTop: isHovered ? "-10px" : "0px",
+                  color: isHovered ? "#fff" : "#000",
+                }}
+              ></i>
             </div>
           );
         })}
